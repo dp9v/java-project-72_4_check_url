5aac629ef9431313a842ffff17bb51fd54337223
dmitrii <polkovnikov13@gmail.com>
2023-06-06 18:35:36 +0200

    init commit

---
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void isEven() {
+        assertTrue(Main.isEven(2));
+    }
+}
