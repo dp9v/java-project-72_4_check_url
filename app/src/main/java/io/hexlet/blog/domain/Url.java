b21d82f97c710c713d902cf1505f2adb08ca748b
dmitrii <polkovnikov13@gmail.com>
2023-07-02 21:07:28 +0200

    Add DB integration

---
+++ app/src/main/java/io/hexlet/blog/domain/Url.java
@@ -0,0 +1,43 @@
+package io.hexlet.blog.domain;
+
+import io.ebean.Model;
+import io.ebean.annotation.WhenCreated;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+public class Url extends Model {
+    @Id
+    private long id;
+
+    private String name;
+
+    @WhenCreated
+    private Instant createdAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
